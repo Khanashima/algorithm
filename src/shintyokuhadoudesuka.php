39982e1be5a57b1d15eaeaddb0e40a33ce5aaf57
hanashima <hana911@hotmail.co.jp>
2015-07-19 19:57:25 +0900

    巷で流行っている「進捗・どう・です・か」をPHPで書いてみた

---
+++ src/shintyokuhadoudesuka.php
@@ -0,0 +1,31 @@
+<?php
+
+/* 
+「進捗・どう・です・か」をランダムに表示し
+「進捗どうですか」が完成したら煽ってくるプログラム
+ */
+
+$source = array('進捗', 'どう', 'です', 'か');
+
+$words = array();
+$n     = 0;
+
+while (true) {
+    $randomWord = $source[rand(0, 3)];
+    $words[]    = $randomWord;
+    echo $randomWord;
+    if ($n > 2 &&
+        $words[$n]   == 'か' &&
+        $words[$n-1] == 'です' &&
+        $words[$n-2] == 'どう' &&
+        $words[$n-3] == '進捗'
+            ) {
+        break;
+    }
+    $n++;
+}
+
+echo '???';
+echo '<br>';
+echo (mb_strlen(implode('', $words))) . ' 文字で煽られた';
+
